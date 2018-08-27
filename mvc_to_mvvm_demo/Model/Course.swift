4bc39b564b82dd7b78da482c7226bb9d9bcf6eec
NateLee <intoqasa@gmail.com>
2018-08-27 15:18:19 +0800

    Added some groups to separate the concerns.

---
+++ mvc_to_mvvm_demo/Model/Course.swift
@@ -0,0 +1,15 @@
+//
+//  Course.swift
+//  mvc_to_mvvm_demo
+//
+//  Created by M78 on 8/27/18.
+//  Copyright © 2018 Brian Voong. All rights reserved.
+//
+
+import Foundation
+
+class Course: Decodable {
+    let id: Int
+    let name: String
+    let number_of_lessons: Int
+}
---
+++ mvc_to_mvvm_demo/ViewModel/CourseViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  CourseViewModel.swift
+//  mvc_to_mvvm_demo
+//
+//  Created by M78 on 8/27/18.
+//  Copyright © 2018 Brian Voong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CourseViewModel { // Attention: It used to be a 'Struct'
+    let name: String
+    
+    // Dependency Injection(DI)
+    init(course: Course) {
+        
+    }
+}
