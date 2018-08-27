6c87a8634e069cf743b98813f6ea8f50e5531548
NateLee <intoqasa@gmail.com>
2018-08-27 15:36:25 +0800

    Added UIColor Extension.

---
+++ mvc_to_mvvm_demo/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  mvc_to_mvvm_demo
+//
+//  Created by M78 on 8/27/18.
+//  Copyright © 2018 Brian Voong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let mainTextBlue = UIColor.rgb(red: 7, green: 71, blue: 89)
+    static let highlightColor = UIColor.rgb(red: 50, green: 199, blue: 242)
+    
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red / 255, green: green / 255, blue: blue / 255, alpha: 1.0)
+    }
+}
---
+++ mvc_to_mvvm_demo/View/CourseCell.swift
@@ -0,0 +1,26 @@
+//
+//  CourseCell.swift
+//  mvc_to_mvvm_demo
+//
+//  Created by M78 on 8/27/18.
+//  Copyright © 2018 Brian Voong. All rights reserved.
+//
+
+import UIKit
+
+class CourseCell: UITableViewCell {
+
+    
+    var courseViewModel: CourseViewModel! // TODO: didSet
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        super.setHighlighted(highlighted, animated: animated)
+        contentView.backgroundColor = isHighlighted ? .highlightColor : .white
+    }
+
+}
